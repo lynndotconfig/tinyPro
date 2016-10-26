a02aa83c6998d13f9d3fc03b7330a2866181247b
lynndotconfig <lynn.config@gmail.com>
2016-10-26 22:38:39 +0800

    [add] input example.

--- pages/basics/basics.js
+++ pages/basics/basics.js
@@ -37,7 +37,11 @@ Page({
 			{name: 'JPN', value: '日本'},
 			{name: 'ENG', value: '英国'},
 			{name: 'FRE', value: '法国'},
-		]
+		],
+
+		//input
+		focus: false,
+		inputValue: ''
 	},
 
 	//text
@@ -84,5 +88,39 @@ Page({
 	},
 	formReset: function(e){
 		console.log("form发送了reset事件")
+	},
+
+	//input
+	bindButtonTap: function(){
+		this.setData({
+			focus: Date.now()
+		})
+	},
+	bindKeyInput: function(e){
+		this.setData({
+			inputValue: e.detail.value
+		})
+	},
+	bindReplaceInput: function(e){
+		var value = e.detail.value
+		var pos = e.detail.cursor
+		if(pos != -1){
+			//光标在中间
+			var left = e.detail.value.slice(0, pos)
+			//计算光标位置
+			pos = left.replace(/11/g, '2').length
+		}
+
+		//直接返回对象，可以对输入进行过滤处理，同时可以控制光标位置
+		return {
+			value: value.replace(/11/g, '2'),
+			cursor: pos
+		}
+	},
+	bindHideKeyboard: function(e){
+		if (e.detail.value === '123'){
+			//收起键盘
+			wx.hideKeyboard()
+		}
 	}
 })
